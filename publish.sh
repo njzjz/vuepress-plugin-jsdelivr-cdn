909e676b51b2d5c37d9ccd0e18dc9088915c5562
a495008890@live.com <a495008890@live.com>
2020-05-09 16:03:38 +0800

    add publish.sh add update package version

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# git config --global user.name "travis"
+# git config --global user.name "travis@miantiao.me"
+
+# setup repo
+rm -rf $REPO_NAME
+git clone https://github.com/$TRAVIS_REPO_SLUG.git
+rm -rf $REPO_NAME/dist
+cp -rf docs/.vuepress/dist $REPO_NAME
+
+# update repo
+cd $REPO_NAME
+git add --all
+git commit -m "update dist"
+git push -f https://$GITHUB_KEY@github.com/$TRAVIS_REPO_SLUG.git HEAD:master
+
+# clean
+rm -rf $REPO_NAME
