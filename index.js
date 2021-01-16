8607885b308d207d30379de25d08c52f74c5623e
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-01-15 20:17:50 -0500

    bugfix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const parser = require('node-html-parser')
 const fs = require('fs')
 const path = require('path')
+import { globby } from '@vuepress/utils'
 
 module.exports = (options, context) => ({
     async onGenerated(app) {
